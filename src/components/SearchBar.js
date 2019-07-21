71c3fe1121fed56b885be2323b8e040135a2ff7a
Casey Hannan <Hannan.Casey@gmail.com>
2019-07-20 20:18:10 -0400

    adding fetch

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,21 +1,28 @@
 import React from 'react'
 
 class SearchBar extends React.Component {
-    onInputChange(event) {
-        console.log(event.target.value)
+    state = {term: ''}
+
+    // onInputChange(event) {
+    //     console.log(event.target.value)
+    // }
+    //axios
+    onFormSubmit(event) {
+        event.preventDefault();
+
+        console.log(this.state.term)
     }
 
-    onInputClick()  {
-        console.log('Input was clicked!')
-    }
-    
     render() {
         return (
         <div className="ui segment">
-            <form className="ui form"> 
+            <form onSubmit={this.onFormSubmit} className="ui form"> 
                 <div className="field">
-                    <label>Image Search</label>
-                    <input type="text" onClick ={this.onInputClick} onChange={this.onInputChange}/>
+                    <label>Image Search</label>                    
+                    <input 
+                    type="text"
+                    value={this.state.term}
+                    onChange={(e) => this.setState({ term: e.target.value })}/>
                 </div>       
             </form>
          </div>
