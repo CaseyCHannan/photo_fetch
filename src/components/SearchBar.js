54f8be0262852d8edfb4c02642a2a5cb69fb26d3
Casey Hannan <Hannan.Casey@gmail.com>
2019-07-22 16:12:16 -0400

    building out api

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,19 +1,9 @@
 import React from 'react'
 
 class SearchBar extends React.Component {
-    // state = {term: ''}
-    constructor(props) {
-        super(props);
-    
-        this.state = { term: "" };
-      }
+    state = {term: ''}
 
-
-    // onInputChange(event) {
-    //     console.log(event.target.value)
-    // }
-    //axios
-    onFormSubmit(event) {
+    onFormSubmit = event => {
         event.preventDefault();
 
         console.log(this.state.term)
@@ -38,31 +28,3 @@ class SearchBar extends React.Component {
 
 export default SearchBar
 
-
-import React, { Component } from "react";
-
-class SearchBar extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = { term: "" };
-  }
-
-  render() {
-    return (
-      <div className="search-bar">
-        <input
-          value={this.state.term}
-          onChange={event => this.onInputChange(event.target.value)}
-        />
-      </div>
-    );
-  }
-
-  onInputChange(term) {
-    this.setState({ term });
-    this.props.onSearchTermChange(term);
-  }
-}
-
-export default SearchBar;
